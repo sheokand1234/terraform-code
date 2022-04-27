c18dd6392c2d8df01de9ee2fbb11e92a3a032012
sheokand1234 <47672203+sheokand1234@users.noreply.github.com>
2022-04-27 12:58:56 +0530

    Update c2-variables.tf

--- c2-variables.tf
+++ c2-variables.tf
@@ -2,12 +2,12 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "us-east-1"
+  default = ""
 }
 
 variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type = string
-  #default = "t2.micro"
+  default = ""
   #default = "t2.small"
 }
